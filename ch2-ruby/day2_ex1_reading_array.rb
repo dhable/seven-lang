43d09241c0d509fe8842139835d694067b8f6e2b
Dan Hable <dhable@gmail.com>
2012-06-23 14:47:44 -0700

    Adding new comment block to file to show the context of the code

--- ch2-ruby/day2_ex1_reading_array.rb
+++ ch2-ruby/day2_ex1_reading_array.rb
@@ -1,8 +1,11 @@
+# Week 1 - Ruby
+# Day 2
+#
+# Problem: Print the contents of an array of sixteen numbers, four
+# numbers at a time, using just each. Now, do the same with each_slice
+# in Enumerable.
+
 numbers = (1..16)
-
-
-# Print the contents of the array, four numbers at a time
-# only using the each method.
 puts "outputting numbers in blocks of 4 using only each"
 tmp = []
 numbers.each do |v|
@@ -16,7 +19,5 @@ end
 puts
 puts
 
-# Print the same contents using the Enumerable#each_slice
-# method instead
 puts "outputting numbers in blocks of 4 using each_slice"
 numbers.each_slice(4) { |s| p s }
