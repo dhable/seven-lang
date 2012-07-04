cdc7bbc8aadc1dba621488d1ea69d6079a34c968
Dan Hable <dhable@gmail.com>
2012-07-03 19:46:32 -0700

    reduced number of sort rules in day2

--- week3-prolog/day2/sort.pl
+++ week3-prolog/day2/sort.pl
@@ -2,10 +2,6 @@ concatenate([], List, List).
 
 concatenate([Head|Tail1], List, [Head|Tail2]) :- concatenate(Tail1, List, Tail2).
 
-sort([], []).
-
-sort([Head|[]], [Head]).
-
 sort([Head1|[Head2|[]]], SortedList)
 	:- concatenate([Head1], [Head2], SortedList), Head1 < Head2.
 
