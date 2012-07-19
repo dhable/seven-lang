5a6b351f7885ec46d010ff829d4645772498d3f1
Dan Hable <dhable@gmail.com>
2012-07-19 15:12:57 -0700

    Updated response to handle cases where the function is called with an object that's not a collection (e.g. a string). In those cases the function will return nil.

--- week6-clojure/day1/collection-type.clj
+++ week6-clojure/day1/collection-type.clj
@@ -4,5 +4,6 @@
 ; Problem: Write a function called (collection-type col) that returns :list, :map or :vector
 ; based on the type of collection col.
 (defn collection-type [col] 
-	(if (list? col) :list
-		(if (map? col) :map :vector)))
+	(if (list? col) :list 
+		(if (map? col) :map 
+			(if (vector? col) :vector nil))))
